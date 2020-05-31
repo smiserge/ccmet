d5d8eba0c4275138c574c149f2df249d5292d1d4
Serguei Smirnov <smiserge@gmail.com>
2020-05-31 16:56:01 -0700

    Updated VTT stream parsing to be more resilient to additional empty strings

--- src/main/java/ccmet/SegmentParser.java
+++ src/main/java/ccmet/SegmentParser.java
@@ -58,52 +58,57 @@ public class SegmentParser {
 
     /**
      * parseStreamOfStrings
+     *
      * @param br
-     * @param srtRecords 
-     * 
+     * @param srtRecords
+     *
      * VTT records are coming as a stream of strings similar to:
-     * --------------------
-     * WEBVTT
-     * X-TIMESTAMP-MAP=LOCAL:00:00:00.000,MPEGTS:0
-     * 
+     * -------------------- WEBVTT X-TIMESTAMP-MAP=LOCAL:00:00:00.000,MPEGTS:0
+     *
      * 2
-     * 03:30.000 --> 03:32.067
-     * The nights are brighter here
-     * than daytime anywhere else!
-     * 
+     * 03:30.000 --> 03:32.067 The nights are brighter here than daytime
+     * anywhere else!
+     *
      * 3
-     * 03:32.133 --> 03:39.667
-     * Lovely Venice!  Abode of every pleasure!
+     * 03:32.133 --> 03:39.667 Lovely Venice! Abode of every pleasure!
      * ----------------------
-     * 
-     * So we process it as follows:
-     * 1. Split the stream into separate groups using empty string as a separator
-     * 2. Parse each group into an SrtRecord object
-     * 3. If valid SrtRecord was possible to create, add it to the collection of SrtRecrods
-     * 4. Bail out when no more strings in the stream
-     * 
+     *
+     * So we process it as follows: 
+     * 1. Split the stream into separate groups using empty string as a separator 
+     * 2. Parse each group into an SrtRecord object 
+     * 3. If valid SrtRecord was possible to create, add it to the collection of SrtRecrods 
+     * 4. Bail out when no more strings are in the stream
+     *
      */
     private void parseStreamOfStrings(BufferedReader br, ArrayList srtRecords) {
         if ((br != null) & (srtRecords != null)) {
 
             try {
-                    String input;
-                    ArrayList srtStrings = new ArrayList<String>();
-                    while ((input = br.readLine()) != null) {
-                        while (!input.isEmpty()) //accumulate until empty string
-                        {
-                            srtStrings.add(input);
-                            input = br.readLine();
-                        }
-                        // empty string found, try to create SRT Record
+                // accumulator of strings in current group
+                ArrayList srtStrings = new ArrayList<String>();
+
+                String input = br.readLine();
+                while (input != null) {
+                    while (!input.isEmpty()) //accumulate until empty string
+                    {
+                        srtStrings.add(input);
                         input = br.readLine();
+                    }
+                    // empty string found 
+                    // create SRT Record if there are any accumulated strings
+                    if (!srtStrings.isEmpty()) {
                         SrtRecord srt = SrtRecord.parse(srtStrings);
                         if (srt != null) // add it if parsing was successful
                         {
                             srtRecords.add(srt);
-                        }                           
-                       //System.out.println(input);
+                        }
+                        // clear the accumulator
+                        srtStrings = new ArrayList<String>();
                     }
+
+                    input = br.readLine();
+                    //System.out.println(input);
+                }
 
             } catch (IOException e) {
                 //e.printStackTrace();
